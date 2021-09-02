8fff15f460a40ea8330207c973407e098ac66cb6
Anna Kijanka <a.kijanka@viewone.pl>
2021-09-02 14:57:33 +0200

    boxes

--- web/app/themes/faq/template-faq.php
+++ web/app/themes/faq/template-faq.php
@@ -3,4 +3,5 @@
 <?php get_header(); ?>
 <?php get_template_part( 'template-parts/template-faq/headline' ); ?>
 <?php get_template_part( 'template-parts/template-faq/accordion' ); ?>
+<?php get_template_part( 'template-parts/template-faq/boxes' ); ?>
 <?php get_footer(); ?>
---
+++ web/app/themes/faq/template-parts/template-faq/boxes.php
@@ -0,0 +1,27 @@
+<section>
+  <div class="container-fluid px-0">
+    <div class="row">
+      <div class="col-12">
+        <img class="w-100 mt-4 mt-lg-10" src="<?php echo get_stylesheet_directory_uri(); ?>/src/images/dark-car.jpg" alt="Ciemny samochód">
+      </div>
+    </div>
+  </div>
+  <div class="container container--narrower position-relative px-0 mt-lg-n13">
+    <div class="row mx-lg-n3">
+      <div class="col-12 col-lg-6">
+        <ul class="list list--check bg-alabaster-09 px-3 py-4 px-lg-6 py-lg-5 mx-lg-3 h-100">
+          <li>Niskie miesięczne raty</li>
+          <li>Szeroki zakres doboru parametrów umowy (opłata wstępna, okres finansowania, limit przebiegu)</li>
+          <li>Elastycznie kształtowany koszt związany z obsługą serwisową</li>
+        </ul>
+      </div>
+      <div class="col-12 col-lg-6">
+        <ul class="list list--check bg-alabaster-09 px-3 py-4 px-lg-6 py-lg-5 mx-lg-3 h-100">
+          <li>Możliwość wymiany auta co 2-5 lat</li>
+          <li>Brak konieczności angażowania kapitału własnego</li>
+          <li>Nienaruszanie zdolności kredytowej</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</section>
